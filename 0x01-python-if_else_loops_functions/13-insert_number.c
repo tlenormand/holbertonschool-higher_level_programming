0f465a41d0b3ab0d3e19aef672996db8712f6b20
tlenormand <lenormand.thomas76110@gmail.com>
2021-12-21 16:34:41 +0000

    correct if head == NULL add node at the beginning - part 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,24 +11,53 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *current = *head;
 
+	if (*head == NULL || number < (*head)->n)
+	{
+		add_nodeint(head, number);
+		return (*head);
+	}
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
-	new->n = number;
-	new->next = NULL;
 
 	if (current == NULL || current->n >= number)
 	{
 		new->next = new;
 		*head = new;
+
 		return (new);
 	}
 
 	while (current && current->next && current->next->n < number)
 		current = current->next;
 
+	new->n = number;
 	new->next = current->next;
 	current->next = new;
+
+	return (new);
+}
+
+
+/**
+* add_nodeint - adding a new node at the beginnig of the linked list
+* @head: addres of the linked list
+* @n: value to add to the linked list
+* Return: address of the new node
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
 	return (new);
 }
