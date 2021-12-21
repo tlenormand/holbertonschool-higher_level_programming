e3a56854343e7b92d80d2369611b0ed8176fc565
tlenormand <lenormand.thomas76110@gmail.com>
2021-12-21 16:08:56 +0000

    correct for negative number - part 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number > 0:
+    last_digit = number % 10
+else:
+    last_digit = (-number % 10) * (-1)
+
 last_digit = abs(number) % 10
+
 print("Last digit of {} is {} ".format(number, last_digit), end = '')
 if last_digit == 0:
     print("and is 0")
