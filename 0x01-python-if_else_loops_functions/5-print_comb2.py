ef74a019c5d3bb4a9f6e6446dfeb6f97f6d930e5
tlenormand <lenormand.thomas76110@gmail.com>
2021-12-21 16:04:59 +0000

    correct correct output - part 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d}, ".format(i), end ='')
-print("{}".format(i))
+for nb in range(0, 99):
+    print("{:02}".format(nb), end =', ')
+print("{}".format(nb + 1))
