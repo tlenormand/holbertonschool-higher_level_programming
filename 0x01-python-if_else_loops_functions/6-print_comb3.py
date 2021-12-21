3cfbc5bc6e6aba737287c54f7a65885d853e3854
tlenormand <lenormand.thomas76110@gmail.com>
2021-12-21 16:12:15 +0000

    correct correct output 01 not 00 - part 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 i = 0
-j = 0
+j = 1
 while i < 8:
     while j < 10:
-        print("{:d}{:d}, ".format(i, j), end ='')
+        print("{:d}{:d}, ".format(i, j), end='')
         j += 1
     i += 1
     j = i + 1
