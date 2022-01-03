90de3ecd54a56f3ca90b9ef717f5bbb38969ef3e
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-03 18:12:29 +0000

    correct if *head == NULL - part 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,7 +10,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *end = *head;
 
-	if (head == NULL || (*head)->next == NULL)
+	if (head == NULL || (*head) == NULL)
 		return (1);
 
 	return (check_palindrome(&end, end));
