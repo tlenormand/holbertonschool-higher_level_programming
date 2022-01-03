a9f17ed5df2f9219e1b7d6059fd1f74adf764509
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-03 14:18:19 +0000

    replace > by >= - part 1, 2, 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ without modifying the original list"""
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     new_list = my_list.copy()
