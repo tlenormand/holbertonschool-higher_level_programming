b71433d4c0ce05a0059f00f1a6f54139f7f76296
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-24 02:09:28 +0000

    correct pycodestyle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -25,37 +25,40 @@ matrix = []
 for i in range(N):
     matrix.append([0] * N)
 
+
 def is_attack(i, j):
-    #checking if there is a queen in row or column
-    for k in range(0,N):
-        if matrix[i][k]==1 or matrix[k][j]==1:
+    # checking if there is a queen in row or column
+    for k in range(0, N):
+        if matrix[i][k] == 1 or matrix[k][j] == 1:
             return True
-    #checking diagonals
-    for k in range(0,N):
-        for l in range(0,N):
-            if (k+l==i+j) or (k-l==i-j):
-                if matrix[k][l]==1:
+    # checking diagonals
+    for k in range(0, N):
+        for lr in range(0, N):
+            if (k + lr == i + j) or (k - lr == i - j):
+                if matrix[k][lr] == 1:
                     return True
     return False
 
+
 def N_queen(n):
-    #if n is 0, solution found
-    if n==0:
+    # if n is 0, solution found
+    if n == 0:
         return True
-    for i in range(0,N):
-        for j in range(0,N):
+    for i in range(0, N):
+        for j in range(0, N):
             '''checking if we can place a queen here or not
             queen will not be placed if the place is being attacked
             or already occupied'''
-            if (not(is_attack(i,j))) and (matrix[i][j]!=1):
+            if (not(is_attack(i, j))) and (matrix[i][j] != 1):
                 matrix[i][j] = 1
-                #recursion
-                #wether we can put the next queen with this arrangment or not
-                if N_queen(n-1)==True:
+                # recursion
+                # wether we can put the next queen with this arrangment or not
+                if N_queen(n - 1) is True:
                     return True
                 matrix[i][j] = 0
 
     return False
+
 
 N_queen(N)
 # for i in matrix:
