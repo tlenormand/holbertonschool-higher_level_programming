7effae1f4e73a1347cea3dbbff263e5ff8dfd731
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-25 12:42:09 +0000

    correct if by elif

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -56,8 +56,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -83,8 +82,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
 
         self.__height = value
