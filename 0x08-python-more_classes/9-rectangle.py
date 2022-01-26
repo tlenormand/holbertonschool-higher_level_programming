f66edd8fa61d656c2eb89c7c62d6058c8fa512aa
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-26 13:15:03 +0000

    inverse 2 lines - part 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -47,8 +47,8 @@ class Rectangle:
 
         Returns: None
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __del__(self):
