90a512d10a64392aec41d9b916a67e979ae205c6
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-31 17:28:00 +0000

    correct if isinstance - part 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -22,4 +22,4 @@ def inherits_from(obj, a_class):
         True: if the object is an instance
         False: otherwise
     """
-    return type(obj) is not a_class
+    return type(obj) is not a_class and isinstance(obj, a_class)
