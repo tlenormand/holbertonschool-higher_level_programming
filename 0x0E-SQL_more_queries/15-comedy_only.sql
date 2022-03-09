429b10a1c70a50c3ab8b5c5be3e188e68f7f263f
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-03-09 17:04:15 +0000

    remove ASC - part 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,4 +5,4 @@ SELECT tv_shows.title FROM `tv_shows`
     JOIN `tv_show_genres` ON tv_shows.id = tv_show_genres.show_id
     JOIN `tv_genres` ON tv_genres.id = tv_show_genres.genre_id
     WHERE tv_genres.name = "Comedy"
-    ORDER BY tv_shows.title ASC;
+    ORDER BY tv_shows.title;
