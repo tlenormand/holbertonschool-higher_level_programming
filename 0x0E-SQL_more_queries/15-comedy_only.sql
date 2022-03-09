ebd06caeeff1f3e40c2693522c90383817b17d49
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-03-09 17:10:56 +0000

    add backtick

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows
 USE `hbtn_0d_tvshows`;
 
-SELECT tv_shows.title FROM `tv_shows`
-    INNER JOIN `tv_show_genres` ON tv_shows.id = tv_show_genres.show_id
-    INNER JOIN `tv_genres` ON tv_genres.id = tv_show_genres.genre_id
-    WHERE tv_genres.name = "Comedy"
-    GROUP BY tv_shows.title
-    ORDER BY tv_shows.title;
+SELECT `tv_shows`.`title` FROM `tv_shows`
+    JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+    JOIN `tv_genres` ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+    WHERE `tv_genres`.`name` = "Comedy"
+    ORDER BY `tv_shows`.`title`;
