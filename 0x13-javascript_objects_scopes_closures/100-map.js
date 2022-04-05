a168f6145145acc8ef8d6870b23bb0453ae63e30
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-05 12:11:19 +0000

    semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -5,7 +5,7 @@ script that imports an array and computes a new array
 const list = require('./100-data.js').list;
 const mapList = [];
 
-list.map((element, idx) => { mapList.push(element * idx); });
+list.map((element, idx) => { return mapList.push(element * idx); });
 
 console.log(list);
 console.log(mapList);
