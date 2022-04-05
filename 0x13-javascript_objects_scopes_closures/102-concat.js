7bcf224db787a24d51860498b6ea04c6ec4964fc
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-05 12:12:31 +0000

    handle error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,13 @@ script that concats 2 files
 const fs = require('fs');
 
 fs.readFile('fileA', 'utf8', (err, fileA) => {
+  if (err) {
+    return console.log(err);
+  }
   fs.readFile('fileB', 'utf8', (err, fileB) => {
+    if (err) {
+      return console.log(err);
+    }
     fs.writeFileSync('fileC', fileA + fileB);
   });
 });
