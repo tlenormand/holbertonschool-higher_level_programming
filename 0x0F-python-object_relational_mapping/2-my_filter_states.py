357329404a54adcc091c54fbfb02a8f8fe6dc010
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-09 19:24:43 +0000

    add order

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4])
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4])
     )
     query_rows = cur.fetchall()
     for row in query_rows:
