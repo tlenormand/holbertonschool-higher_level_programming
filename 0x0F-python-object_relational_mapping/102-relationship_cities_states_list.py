45dbebac2b23bc5afaa00672aefda0cfe81a6406
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-10 12:17:35 +0000

    add task 17

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all City objects from the database hbtn_0e_101_usa
+"""
+import sys
+from relationship_state import State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    session = Session(engine)
+    query = session.query(City, State).filter(City.state_id == State.id).order_by(City.id).all()
+    for city, state in query:
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
+
+    session.close()
