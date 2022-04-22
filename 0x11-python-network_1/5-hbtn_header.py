15a6ea5d8cc6c174527f6d27777f4d90edf57c97
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 09:14:05 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python script that sends a request to the URL'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
