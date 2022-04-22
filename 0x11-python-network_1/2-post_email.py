0e858c38002000edd334e8f277a1822bdec4036b
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 08:49:11 +0000

    correct code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('utf-8')
+    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode('utf-8')
 
-    with urllib.request.urlopen(sys.argv[1], data) as response:
-        html = response.info()
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
 
-    print("Your email is:", html)
+    print(html.decode('utf-8'))
