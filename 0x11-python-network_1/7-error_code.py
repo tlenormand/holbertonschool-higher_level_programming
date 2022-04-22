69b7ca400b6bbd07216ac79d2a99eb41c3f15ed3
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 11:37:09 +0000

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Python script that sends a request to the URL'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.post(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code:", res.status_code)
+    else:
+        print(res.read())
