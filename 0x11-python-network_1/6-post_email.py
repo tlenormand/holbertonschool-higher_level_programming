4bc83d17e987f0e9d98f4aca35aba1ce252fa72a
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 09:24:56 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Python script that sends a request to the URL'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    data = {'email' : sys.argv[2]}
+    res = requests.post(sys.argv[1], data=data).text
+
+    print(res)
