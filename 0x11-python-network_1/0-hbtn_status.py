d1c2b3fcc83eeca942152ad98dcc0ce8aa8605c5
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 08:36:59 +0000

    use tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
 print("Body response:")
-print('    - type:', type(html))
-print('    - content:', html)
-print('    - utf8 content:', html.decode('utf-8'))
+print('\t- type:', type(html))
+print('\t- content:', html)
+print('\t- utf8 content:', html.decode('utf-8'))
