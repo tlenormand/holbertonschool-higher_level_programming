2cccc64b6106b9490b00433010cedec9dcba574b
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 08:34:49 +0000

    remove space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
 print("Body response:")
-print('    - type: ', type(html))
-print('    - content: ', html)
-print('    - utf8 content: ', html.decode('utf-8'))
+print('    - type:', type(html))
+print('    - content:', html)
+print('    - utf8 content:', html.decode('utf-8'))
