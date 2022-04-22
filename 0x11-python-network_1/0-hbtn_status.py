75515f936ff1453039d514b534677ac6640e7f0d
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 08:33:26 +0000

    add body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
+print("Body response:")
 print('    - type: ', type(html))
 print('    - content: ', html)
 print('    - utf8 content: ', html.decode('utf-8'))
