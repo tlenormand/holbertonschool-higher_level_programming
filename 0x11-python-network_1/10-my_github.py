35a34e41be3537181207c3d843f0535b13ae3b5d
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 12:09:10 +0000

    add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python script that sends a request to the URL'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    print(res.json().get("id"))
