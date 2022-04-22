2e20dfa6ad5c148f72943c6d0c9e25ae34de5f04
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-22 08:30:39 +0000

    task 0 to 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Python script that sends a POST request to the passed URL'''
+import sys
+import urllib.request
+
+
+data = urllib.parse.urlencode({"email": sys.argv[2]})
+data = data.encode('utf-8')
+
+with urllib.request.urlopen(sys.argv[1], data) as response:
+    html = response.info()
+
+print(html)
